514af3a5aacd45995a9a344621db1482644eb54c
shaimaamorsi <smorsi@seek.com.au>
2020-01-28 23:45:12 +0000

    config changes

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_fJq8YzeDP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '43ph5o44od7brv2bi8add07gdi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
